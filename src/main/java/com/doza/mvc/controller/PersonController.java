ea2bb9a52cb12ece1cafe582c68ded874d983cbb
Daniil <DozDi@yandex.ru>
2023-11-03 13:22:07 +0300

    Add Person and GET/POST with person

---
+++ src/main/java/com/doza/mvc/controller/PersonController.java
@@ -0,0 +1,32 @@
+package com.doza.mvc.controller;
+
+import com.doza.mvc.model.Person;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PersonController {
+
+
+    @GetMapping("/showForm")
+    public String showForm(Model model) {
+
+        Person thePerson = new Person();
+
+        model.addAttribute("person", thePerson);
+
+        return "person-show-form";
+    }
+
+    @PostMapping("/processPersonForm")
+    public String processPersonForm(@ModelAttribute("person") Person person) {
+
+        System.out.println("Person : " + person.getFirstName() + " " + person.getLastName() + "!");
+
+        return "person-confirmation";
+    }
+}
