3e2b0c7c7c512a4483e9a4e9a9601eb11d404043
Daniil <DozDi@yandex.ru>
2023-11-07 12:04:44 +0300

    add validations

--- src/main/java/com/doza/mvc/controller/PersonController.java
+++ src/main/java/com/doza/mvc/controller/PersonController.java
@@ -1,10 +1,15 @@
 package com.doza.mvc.controller;
 
 import com.doza.mvc.model.Person;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -19,6 +24,15 @@ public class PersonController {
 
     @Value("${programmingLanguages}")
     private List<String> languages;
+    @Value("${systems}")
+    private List<String> systems;
+
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder) {
+
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
 
 
     @GetMapping("/showForm")
@@ -27,17 +41,25 @@ public class PersonController {
         Person thePerson = new Person();
 
         model.addAttribute("person", thePerson);
-        model.addAttribute("countries", countries);
-        model.addAttribute("languages", languages);
+//        model.addAttribute("countries", countries);
+//        model.addAttribute("languages", languages);
+//        model.addAttribute("systems", systems);
 
         return "person-show-form";
     }
 
     @PostMapping("/processPersonForm")
-    public String processPersonForm(@ModelAttribute("person") Person person) {
+    public String processPersonForm(@Valid @ModelAttribute("person") Person person,
+                                    BindingResult bindingResult) {
 
         System.out.println("Person : " + person.getFirstName() + " " + person.getLastName() + "!");
+        System.out.println("Binding result: " + bindingResult.toString());
+        System.out.println("\n\n\n\n");
 
-        return "person-confirmation";
+        if(bindingResult.hasErrors()) {
+          return "person-show-form";
+        } else {
+            return "person-confirmation";
+        }
     }
 }
--- src/main/java/com/doza/mvc/model/Person.java
+++ src/main/java/com/doza/mvc/model/Person.java
@@ -1,16 +1,34 @@
 package com.doza.mvc.model;
 
+import jakarta.validation.constraints.*;
+
+import java.util.List;
+
 public class Person {
 
+    @NotNull(message = "is required")
+    @Size(min = 1, message = "is required")
     private String firstName;
-    private String lastName;
-    private String country;
 
+    @NotNull(message = "is required")
+    @Size(min = 1, message = "is required")
+    private String lastName;
+
+    @NotNull(message = "is required")
+    @Min(value = 0, message = "Minimal >= 0")
+    @Max(value = 10, message = "Maximum <= 10")
+    private Integer freePasses;
+
+    @NotNull(message = "is required")
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chats or digits")
+    private String postalCode;
+
+    private String country;
     private String favoriteLanguage;
+    private List<String> favoriteSystems;
 
     public Person() {
     }
-
 
     public String getFirstName() {
         return firstName;
@@ -28,6 +46,22 @@ public class Person {
         this.lastName = lastName;
     }
 
+    public Integer getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(Integer freePasses) {
+        this.freePasses = freePasses;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
     public String getCountry() {
         return country;
     }
@@ -42,5 +76,13 @@ public class Person {
 
     public void setFavoriteLanguage(String favoriteLanguage) {
         this.favoriteLanguage = favoriteLanguage;
+    }
+
+    public List<String> getFavoriteSystems() {
+        return favoriteSystems;
+    }
+
+    public void setFavoriteSystems(List<String> favoriteSystems) {
+        this.favoriteSystems = favoriteSystems;
     }
 }
